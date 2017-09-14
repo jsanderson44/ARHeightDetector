b625bd77576a987db044b16d158415e7872218bf
John Sanderson <john.sanderson@theappbusiness.com>
2017-09-14 10:38:07 +0100

    ++

--- ExampleARKitApp/ExampleARKitApp/ViewController.swift
+++ ExampleARKitApp/ExampleARKitApp/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
   @IBOutlet fileprivate var sceneView: ARSCNView!
   @IBOutlet fileprivate var distanceLabel: UILabel!
   @IBOutlet fileprivate var addButton: UIButton!
+  @IBOutlet fileprivate var instructionLabel: UILabel!
   
-  var planes: [ARPlaneAnchor : SCNNode] = [:]
+  fileprivate var planes: [ARPlaneAnchor : SCNNode] = [:]
   fileprivate var squareNodes: [SCNNode] = []
   fileprivate var cylinderNode: CylinderLine?
   fileprivate var currentFaceView: UIView?
+  fileprivate var pollingTimer: Timer?
   
   fileprivate var shouldAddVerticalOffset: Bool = false
   
@@ -28,13 +30,22 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     sceneView.delegate = self
     sceneView.showsStatistics = true
-    
-    startPollingForFaceDetection()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
+    createAndStartNewSession()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  fileprivate func createAndStartNewSession() {
     // Create a session configuration
     let configuration = ARWorldTrackingConfiguration()
     configuration.planeDetection = .horizontal
@@ -44,13 +55,6 @@ class ViewController: UIViewController {
     sceneView.session.run(configuration)
     sceneView.automaticallyUpdatesLighting = true
     sceneView.debugOptions = [.showWireframe]
-  }
-  
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    
-    // Pause the view's session
-    sceneView.session.pause()
   }
   
   @IBAction fileprivate func didTapReset() {
@@ -82,7 +86,19 @@ class ViewController: UIViewController {
   }
   
   fileprivate func startPollingForFaceDetection() {
-    Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(startFaceTracking), userInfo: nil, repeats: true)
+    DispatchQueue.main.async {
+      if self.instructionLabel.alpha == 0.0 {
+        self.instructionLabel.text = "Floor detected! Please focus on the face of the person you would like to measure!"
+        self.instructionLabel.showViewWithAnimation()
+        self.pollingTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(self.startFaceTracking), userInfo: nil, repeats: true)
+      }
+    }
+    
+//    pollingTimer?.fire()
+  }
+  
+  fileprivate func stopPollingForFaceDetection() {
+    pollingTimer?.invalidate()
   }
   
   fileprivate func insertGeomerty(_ hitResult: ARHitTestResult) {
@@ -108,7 +124,6 @@ class ViewController: UIViewController {
       addButton.isEnabled = false
       addButton.backgroundColor = UIColor.lightGray
       
-      //      let distance = node.position.distanceFrom(endPosition: (squareNodes.first?.position)!)
       let distance = node.position.verticalDistance(endPosition: (squareNodes.first?.position)!)
       let distanceString = String(format: "%.2f", distance)
       distanceLabel.text = "Distance: \(distanceString)m"
@@ -131,11 +146,8 @@ class ViewController: UIViewController {
     // SCNPlanes are vertically oriented in their local coordinate space.
     // Rotate it to match the horizontal orientation of the ARPlaneAnchor.
     planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-    
     planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
-    
     planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: plane, options: nil))
-    
     planes[anchor] = planeNode
     
     return planeNode
@@ -161,7 +173,6 @@ class ViewController: UIViewController {
         print("No face observations")
         return
       }
-    
       self.drawVisionRequestResults(observations)
     }
     
@@ -169,25 +180,37 @@ class ViewController: UIViewController {
   }
   
   func drawVisionRequestResults(_ results: [VNFaceObservation]) {
-    print("face count = \(results.count) ")
+    print("Number of faces: \(results.count)")
+    if results.count > 0 {
+      DispatchQueue.main.async {
+        self.instructionLabel.hideViewWithAnimation()
+        self.stopPollingForFaceDetection()
+      }
+    }
     
-    let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.view.frame.height)
+//    let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.view.frame.height)
+//    let translate = CGAffineTransform.identity.scaledBy(x: self.view.frame.width, y: self.view.frame.height)
     
-    let translate = CGAffineTransform.identity.scaledBy(x: self.view.frame.width, y: self.view.frame.height)
-    
-    for face in results {
+    guard let face = results.first else { return }
       // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
-      let facebounds = face.boundingBox.applying(translate).applying(transform)
-      self.drawView(around: facebounds)
-      
-      
+//      let facebounds = face.boundingBox.applying(translate).applying(transform)
+//      self.drawView(around: facebounds)
       let boundingBox = self.transformBoundingBox(face.boundingBox)
       guard let worldCoord = self.normalizeWorldCoord(boundingBox) else { return }
-      
-      let distance = worldCoord.verticalDistance(endPosition: (squareNodes.first?.position)!)
-      distanceLabel.text = "Distance: \(distance)"
-      print("!!!!! \(distance) !!!!!")
-    }
+      drawNodeAround(worldCoord: worldCoord, boundingBox: boundingBox)
+  }
+  
+  fileprivate func drawNodeAround(worldCoord: SCNVector3, boundingBox: CGRect) {
+    let sphere = SCNSphere(radius: 0.004)
+    sphere.firstMaterial?.diffuse.contents = UIColor.gray
+    let sphereNode = SCNNode(geometry: sphere)
+    sphereNode.opacity = 0.6
+    
+    sphereNode.position = SCNVector3Make(worldCoord.x, worldCoord.y, worldCoord.z)
+    sceneView.scene.rootNode.addChildNode(sphereNode)
+    
+    let distance = sphereNode.position.verticalDistance(endPosition: (planes.first?.value.position)!)
+    distanceLabel.text = "Height: \(distance)"
   }
   
   func drawView(around rect: CGRect) {
@@ -239,7 +262,6 @@ class ViewController: UIViewController {
     
     // Filter results that are to close
     if let closestResult = arHitTestResults.filter({ $0.distance > 0.10 }).first {
-      //            print("vector distance: \(closestResult.distance)")
       return SCNVector3.positionFromTransform(closestResult.worldTransform)
     }
     return nil
@@ -252,30 +274,72 @@ class ViewController: UIViewController {
   private func transformBoundingBox(_ boundingBox: CGRect) -> CGRect {
     var size: CGSize
     var origin: CGPoint
-    switch UIDevice.current.orientation {
-    case .landscapeLeft, .landscapeRight:
-      size = CGSize(width: boundingBox.width * self.view.bounds.height,
-                    height: boundingBox.height * self.view.bounds.width)
-    default:
-      size = CGSize(width: boundingBox.width * self.view.bounds.width,
-                    height: boundingBox.height * self.view.bounds.height)
-    }
     
     switch UIDevice.current.orientation {
     case .landscapeLeft:
+      size = CGSize(width: boundingBox.width * self.view.bounds.height,
+                    height: boundingBox.height * self.view.bounds.width)
       origin = CGPoint(x: boundingBox.minY * self.view.bounds.width,
                        y: boundingBox.minX * self.view.bounds.height)
     case .landscapeRight:
+      size = CGSize(width: boundingBox.width * self.view.bounds.height,
+                    height: boundingBox.height * self.view.bounds.width)
       origin = CGPoint(x: (1 - boundingBox.maxY) * self.view.bounds.width,
                        y: (1 - boundingBox.maxX) * self.view.bounds.height)
     case .portraitUpsideDown:
+      size = CGSize(width: boundingBox.width * self.view.bounds.width,
+                    height: boundingBox.height * self.view.bounds.height)
       origin = CGPoint(x: (1 - boundingBox.maxX) * self.view.bounds.width,
                        y: boundingBox.minY * self.view.bounds.height)
     default:
+      size = CGSize(width: boundingBox.width * self.view.bounds.width,
+                    height: boundingBox.height * self.view.bounds.height)
       origin = CGPoint(x: boundingBox.minX * self.view.bounds.width,
                        y: (1 - boundingBox.maxY) * self.view.bounds.height)
     }
     
     return CGRect(origin: origin, size: size)
+  }
+}
+
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+  
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    // This visualization covers only detected planes.
+    guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    
+    let planeNode = createPlaneNode(anchor: planeAnchor)
+    
+    // ARKit owns the node corresponding to the anchor, so make the plane a child node.
+    node.addChildNode(planeNode)
+    print("Added plane. Now starting face detection.")
+    
+    DispatchQueue.main.async {
+      if self.instructionLabel.alpha == 1.0 {
+        self.instructionLabel.hideViewWithAnimation()
+      }
+    }
+    
+    startPollingForFaceDetection()
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    
+    // Remove existing plane nodes
+    planes[planeAnchor]?.removeFromParentNode()
+    
+    
+    let planeNode = createPlaneNode(anchor: planeAnchor)
+    
+    node.addChildNode(planeNode)
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+    guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    
+    // Remove existing plane nodes
+    planes[planeAnchor]?.removeFromParentNode()
   }
 }
