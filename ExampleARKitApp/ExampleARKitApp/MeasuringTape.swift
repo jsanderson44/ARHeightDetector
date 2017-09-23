c3929a47ca8959e605e89a094c049a6e9988d254
John Sanderson <johnsanderson@johns-mbp.home>
2017-09-23 22:36:42 +0100

    ++

--- ExampleARKitApp/ExampleARKitApp/MeasuringTape.swift
+++ ExampleARKitApp/ExampleARKitApp/MeasuringTape.swift
@@ -11,52 +11,57 @@ import SceneKit
 
 class MeasuringTape: SCNNode {
   
-  init(parent: SCNNode, startPoint: SCNVector3, endPoint: SCNVector3, width: CGFloat = 0.01) {
+  private let newParent: SCNNode?
+  private let startPoint: SCNVector3?
+  private let endPoint: SCNVector3?
+  private let oneCentimeter: CGFloat = 0.01
+  
+  init(parent: SCNNode, startPoint: SCNVector3, endPoint: SCNVector3) {
+    self.newParent = parent
+    self.startPoint = startPoint
+    self.endPoint = endPoint
     super.init()
     
-    //Calculate the height of our line
-    let  height = startPoint.distanceFrom(endPosition: endPoint)
-    
-    //set position to v1 coordonate
+    setup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setup() {
+    guard let startPoint = startPoint,
+      let endPoint = endPoint else { return }
     position = startPoint
-    
-    //Create the second node to draw direction vector
     let nodeV2 = SCNNode()
-    
-    //define his position
     nodeV2.position = endPoint
-    //add it to parent
-    parent.addChildNode(nodeV2)
+    newParent?.addChildNode(nodeV2)
     
-    //Align Z axis
-    let zAlign = SCNNode()
-    zAlign.eulerAngles.x = .pi / 2
+    configureTapeNodes()
     
-    //create our cylinder
-    let heightInCm = height*100
-    let roundedDownHeight = floor(heightInCm)
-    guard roundedDownHeight > 0 else { return }
-    for i in 1...Int(roundedDownHeight) {
-        let plane = SCNBox(width: width, height: 0.01, length: 0.001, chamferRadius: 0)
-        plane.firstMaterial?.diffuse.contents = UIImage(named: "MeasureTape")
-        let planeNode = SCNNode(geometry: plane)
-        let offset = (Double(i-1) * 0.01) + 0.005 //TODO
-        planeNode.position.y = Float(-(height/2))// + Float(offset))
-        zAlign.addChildNode(planeNode)
-    }
-
-    //Add it to child
-    addChildNode(zAlign)
-    
-    //set constraint direction to our vector
     constraints = [SCNLookAtConstraint(target: nodeV2)]
   }
   
-  override init() {
-    super.init()
-  }
+  private func configureTapeNodes() {
+    guard let startPoint = startPoint,
+      let endPoint = endPoint else { return }
+    let  height = startPoint.distanceFrom(endPosition: endPoint)
+    let zAlign = SCNNode()
+    zAlign.eulerAngles.x = .pi / 2
     
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
+    let heightInCm = height*100
+    let roundedDownHeight = ceil(heightInCm)
+    guard roundedDownHeight > 0 else { return }
+    for i in 1...Int(roundedDownHeight) {
+      let plane = SCNBox(width: oneCentimeter, height: oneCentimeter, length: 0.001, chamferRadius: 0)
+      plane.firstMaterial?.diffuse.contents = UIImage(named: "MeasureTape")
+      let planeNode = SCNNode(geometry: plane)
+      let multiplier = CGFloat(i - 1)
+      let offset = (multiplier * oneCentimeter) + (oneCentimeter / 2)
+      planeNode.position.y = Float(-(height) + Float(offset))
+      zAlign.addChildNode(planeNode)
+    }
+    
+    addChildNode(zAlign)
   }
 }
